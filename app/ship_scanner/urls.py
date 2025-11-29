e07e3fed832d6f6d3247845643dd34b1d075a3ce
Karam Shukurlu <146780222+shukurlukerem@users.noreply.github.com>
2025-11-29 23:11:42 +0400

    fix: core urls.py

--- app/ship_scanner/urls.py
+++ app/ship_scanner/urls.py
@@ -1,21 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
     SpectacularRedocView
 )
+
 from scanner.views import scan_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scan/', scan_view, name='scan'),
 
-    # API docs (Swagger + Redoc)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-    # scanner app API-lər
     path('api/', include("scanner.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
