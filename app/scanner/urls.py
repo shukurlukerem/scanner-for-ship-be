a04f128362c2836fd15d0446df961dfa3ed1c4eb
Karam Shukurlu <146780222+shukurlukerem@users.noreply.github.com>
2025-11-30 17:51:54 +0400

    feat: excel export

--- app/scanner/urls.py
+++ app/scanner/urls.py
@@ -1,6 +1,6 @@
 # app/urls.py
 from django.urls import path
-from .views import GenerateQRAPIView,ScanAPIView, TodayLogsAPIView,WorkerListAPIView, ScannedUsersListAPIView
+from .views import GenerateQRAPIView,ScanAPIView, TodayLogsAPIView,WorkerListAPIView, ScannedUsersListAPIView,ExportTodayExcelAPIView
 
 urlpatterns = [
     path("generate-qr/", GenerateQRAPIView.as_view(), name="generate-qr"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("today-logs/", TodayLogsAPIView.as_view(), name="today-logs"),
     path("workers/", WorkerListAPIView.as_view(), name="worker-list"),
     path("scanned-users/", ScannedUsersListAPIView.as_view(), name="scanned-users"),
+    path("export-excel/", ExportTodayExcelAPIView.as_view(), name="export-excel"),
 ]
--- app/scanner/views.py
+++ app/scanner/views.py
@@ -7,6 +7,9 @@ from rest_framework import generics
 from drf_spectacular.utils import extend_schema
 from django.utils import timezone
 from datetime import timedelta
+from django.http import HttpResponse
+from openpyxl import Workbook
+
 
 from .serializers import WorkerCreateSerializer, WorkerListSerializer,ScannerLogListSerializer
 from .pagination import DefaultPagination
@@ -70,12 +73,11 @@ class TodayLogsAPIView(APIView):
 
 class ScanAPIView(APIView):
     def post(self, request):
-        qr_text = request.data.get("qr_text")  # front bunu göndərir
+        qr_text = request.data.get("qr_text")  
 
         if not qr_text:
             return Response({"detail": "QR kod göndərilməyib"}, status=400)
 
-        # Gələn mətn "WORKER_ID:12" formatındadır → id-ni çıxarırıq
         if not qr_text.startswith("WORKER_ID:"):
             return Response({"detail": "QR formatı yanlışdır"}, status=400)
 
@@ -85,7 +87,6 @@ class ScanAPIView(APIView):
         if not worker:
             return Response({"detail": "Bu işçi tapılmadı"}, status=404)
 
-        # Eyni gün giriş-çıxış limitləri
         now = timezone.now()
         start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
         end_of_day = start_of_day + timedelta(days=1)
@@ -133,7 +134,7 @@ class ScannedUsersListAPIView(generics.ListAPIView):
     def get(self, request):
         logs = ScannerLog.objects.select_related("worker").order_by("scanned_at")
 
-        grouped = {}  # worker_id -> data
+        grouped = {}  
 
         for log in logs:
             worker_id = log.worker.id
@@ -156,3 +157,61 @@ class ScannedUsersListAPIView(generics.ListAPIView):
                 grouped[key]["exit_time"] = log.scanned_at.strftime("%H:%M:%S")
 
         return Response(list(grouped.values()))
+
+
+class ExportTodayExcelAPIView(APIView):
+    def get(self, request):
+        now = timezone.now()
+        start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        end_of_day = start_of_day + timedelta(days=1)
+
+        logs = ScannerLog.objects.select_related("worker").filter(
+            scanned_at__gte=start_of_day,
+            scanned_at__lt=end_of_day
+        ).order_by("worker_id", "scanned_at")
+
+        # --- GÜNƏ GÖRƏ QRUPLAŞDIR ---
+        grouped = {}
+
+        for log in logs:
+            worker_id = log.worker.id
+            date_str = log.scanned_at.strftime("%Y-%m-%d")
+
+            key = f"{worker_id}_{date_str}"
+
+            if key not in grouped:
+                grouped[key] = {
+                    "worker_name": log.worker.full_name,
+                    "date": date_str,
+                    "entry_time": None,
+                    "exit_time": None
+                }
+
+            if log.scan_type == "entry":
+                grouped[key]["entry_time"] = log.scanned_at.strftime("%H:%M:%S")
+            elif log.scan_type == "exit":
+                grouped[key]["exit_time"] = log.scanned_at.strftime("%H:%M:%S")
+
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Giriş-Çıxış Cədvəli"
+
+        ws.append(["Name Surname", "Date", "Entry Time", "Exit Time"])
+
+        for row in grouped.values():
+            ws.append([
+                row["worker_name"],
+                row["date"],
+                row["entry_time"] or "-",
+                row["exit_time"] or "-"
+            ])
+
+        response = HttpResponse(
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+
+        response["Content-Disposition"] = 'attachment; filename="today_logs.xlsx"'
+
+        wb.save(response)
+
+        return response
